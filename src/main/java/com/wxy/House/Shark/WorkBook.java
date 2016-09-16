0f22e8c3971aa7271ce7eff87f773a00a130eb51
wxy <26982912@qq.com>
2016-09-16 10:52:35 +0800

    wxy

--- src/main/java/com/wxy/House/Shark/WorkBook.java
+++ src/main/java/com/wxy/House/Shark/WorkBook.java
@@ -64,17 +64,32 @@ public class WorkBook {
 
         try {
 
-            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='25'");
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='25' ORDER BY PRIORITY");
             sqlWriter.write("use DB_PLAT_SYSTEM;");
             sqlWriter.newLine();
-            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='house.useType';");
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='house.useType';");//设计用途
             sqlWriter.newLine();
-            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='28';");
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='28';");//产权来源
             sqlWriter.newLine();
-            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='house.structure';");
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='house.structure';");//结构
             sqlWriter.newLine();
-            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='26';");
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='26';");//产别
             sqlWriter.newLine();
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='house.houseType';");//房屋性质
+            sqlWriter.newLine();
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='67';");//共有权人与产权人关系
+            sqlWriter.newLine();
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='55';");//土地取得方式
+            sqlWriter.newLine();
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='32';");//土地性质
+            sqlWriter.newLine();
+            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='54';");//付款方式---枚举
+            sqlWriter.newLine();
+//            sqlWriter.write("DELETE FROM WORD WHERE CATEGORY='70';");//开发商资质等级 不需要
+//            sqlWriter.newLine();
+
+
+
             sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
             while (resultSet.next()){
                 sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("house.useType"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
@@ -94,6 +109,195 @@ public class WorkBook {
             return;
 
         }
+
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='28' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 产权来源" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("28"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("28产权来源 is complate");
+
+        } catch (Exception e) {
+            System.out.println("28产权来源 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='27' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 结构" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("house.structure"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("house.structure结构 is complate");
+
+        } catch (Exception e) {
+            System.out.println("house.structure结构 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='26' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 产别" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("26"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("26产别 is complate");
+
+        } catch (Exception e) {
+            System.out.println("26产别 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='64' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 房屋性质" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("house.houseType"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("house.houseType房屋性质 is complate");
+
+        } catch (Exception e) {
+            System.out.println("house.houseType房屋性质 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='67' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 共有权人与产权人关系" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("67"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("67共有权人与产权人关系 is complate");
+
+        } catch (Exception e) {
+            System.out.println("67共有权人与产权人关系 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='55' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 土地取得方式" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("55"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("55土地取得方式 is complate");
+
+        } catch (Exception e) {
+            System.out.println("55土地取得方式 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
+
+        try {
+
+            resultSet = statement.executeQuery("SELECT * FROM DGWordBook where TypeID='32' ORDER BY PRIORITY");
+            sqlWriter.newLine();
+            sqlWriter.write("-- 土地性质" );
+            sqlWriter.newLine();
+            sqlWriter.write("INSERT WORD (ID, _KEY, _VALUE, CATEGORY, DESCRIPTION, PRIORITY, ENABLE) VALUES " );
+            while (resultSet.next()){
+                sqlWriter.write("("+ Q.v(Q.p(resultSet.getString("ID")), Q.p("0"), Q.p(resultSet.getString("Value")), Q.p("32"), Q.p(""),Q.p(resultSet.getString("Priority")),"True"));
+                if(resultSet.isLast()) {
+                    sqlWriter.write(");");
+                }else {
+                    sqlWriter.write("),");
+                }
+
+            }
+            sqlWriter.flush();
+            System.out.println("32土地性质 is complate");
+
+        } catch (Exception e) {
+            System.out.println("32土地性质 is errer");
+            e.printStackTrace();
+            return;
+
+        }
+
 
     }
 
